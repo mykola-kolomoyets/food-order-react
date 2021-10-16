e661ba9dc9a00c77dca6f3835ecf8012746d6abf
Mykola Kolomoyets <kolomoyets473@gmail.com>
2021-10-16 22:52:54 +0300

    v0.1.0 beginning

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './App.css';
+
+const App: FC = () => {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
+  );
+}
+
+export default App;
