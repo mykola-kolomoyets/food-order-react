3fee453c54c385c323e1f498822c9a5ae9a27190
Mykola Kolomoyets <kolomoyets473@gmail.com>
2021-10-16 23:18:27 +0300

    v0.2.1 scss moduling configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React, { FC } from 'react';
+import React, { FC, Fragment } from 'react';
 import './App.css';
 
 import { Header } from '@components';
 
 const App: FC = () => {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      <Header></Header>
+    </Fragment>
   );
 }
 
--- src/components/ui/header/header.component.tsx
+++ src/components/ui/header/header.component.tsx
@@ -1,17 +1,17 @@
 import { FC, Fragment } from 'react';
 
-import './header.scss';
+import styles from './header.module.scss';
 
 import Meals from '@img/meals.jpg';
 
 const Header: FC = props => {
   return (
     <Fragment>
-      <header>
+      <header className={styles.header}>
         <h1>ReactMeals</h1>
         <button>Cart</button>
       </header>
-      <section>
+      <section className={styles.mainImage}>
         <img src={Meals} alt="meals" />  
       </section> 
     </Fragment>
